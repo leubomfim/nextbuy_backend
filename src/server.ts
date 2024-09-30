e553a538aee295b896fc06044d9f11651c4754db
UsuarioDeCreme <leosoares.me2@gmail.com>
2024-09-30 15:49:00 -0300

    change diversous fastify plugins version

--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,12 @@
 import Fastify from "fastify";
 import { FastifyRequest, FastifyReply } from "fastify";
 import { routes } from "./routes";
-import { veifyJwt } from "./lib/jwt";
-import { RateLimiterMemory } from 'rate-limiter-flexible';
 import fastifyCookie from "@fastify/cookie";
+import { veifyJwt } from "./lib/jwt";
 import helmet from "@fastify/helmet";
+import fastifyCors from "@fastify/cors";
 
 const app = Fastify({ logger: true });
-
-const rateLimiter = new RateLimiterMemory({
-  points: 5, // Número de requisições permitidas
-  duration: 1, // Por segundo
-})
-
-app.addHook('onRequest', async (request, reply) => {
-  try {
-    await rateLimiter.consume(request.ip);
-  } catch (err) {
-    reply.status(429).send('Muitas requisições');
-  }
-});
-
-app.addHook('onSend', (request: any, reply: any, payload: any, done) => {
-  reply.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL || 'http://localhost:5173');
-  reply.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  reply.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  reply.header('Access-Control-Allow-Credentials', 'true');
-  done();
-})
-
-app.options('/*', (request, reply) => {
-  reply.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL || 'http://localhost:5173');
-  reply.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  reply.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  reply.header('Access-Control-Allow-Credentials', 'true');
-  reply.send();
-});
 
 app.addHook("preHandler", async (req: FastifyRequest, reply: FastifyReply) => {
   const token = req.cookies.token;
@@ -57,9 +28,19 @@ app.setErrorHandler((error, req, reply) => {
 
 const start = async () => {
   await app.register(routes);
+  app.register(fastifyCors, {
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173', // Ajuste para a URL de produção
+    credentials: true // Permite o envio de cookies
+  });
   await app.register(fastifyCookie, {
     secret: process.env.COOKIE_SECRET,
     parseOptions: {},
+  });
+  await app.register(import("@fastify/rate-limit"), {
+    max: 10,
+    timeWindow: 5000,
+    cache: 10000,
+    hook: "preHandler",
   });
   await app.register(helmet, {
     contentSecurityPolicy: {
